cdcd0bd5b60544761d68aca27be2fd48c4a70dca
Nazhdaque <nazhdaque@gmail.com>
2021-05-04 10:53:22 +0300

    violet blocks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-$(".btn").click(function(e){
-    let x = $(this).attr('data-btn');
-    $("html, body").animate({
-        scrollTop: $(`#num_${x}`).offset().top
-    }, 250);
+$(".btn").click(function (e) {
+    let x = $(this).attr("data-btn");
+    $("html, body").animate(
+        {
+            scrollTop: $(`#num_${x}`).offset().top,
+        },
+        250
+    );
 });
 
-$(".num div").click(function(e){
-    $("html, body").animate({
-        scrollTop: 0
-    }, 250);
+$(".num div").click(function (e) {
+    $("html, body").animate(
+        {
+            scrollTop: 0,
+        },
+        250
+    );
 });
